3edc8c6de097ef0f01a8dcc84a9a86fd995dbeeb
Kira Marston <kiramarston@gmail.com>
2020-04-13 17:55:36 +0100

    Professor Ruby in the terminal with the Javascript.

---
+++ whodunit.js
@@ -0,0 +1,228 @@
+// EPISODE ONE
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I would expect 'The murderer is Miss Scarlet' to be returned.
+
+
+// EPISODE TWO
+
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I would expect an error for attempting to change the murderer const, and the
+// murderer will not be changed.
+
+
+// EPISODE THREE
+
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// I would expect the first verdict to return Mrs. Peacock as the murderer,
+// as it calls the declareMurderer function, where she is defined at the murderer.
+
+// I would expect the second verdict to return Professor Plum, as it doesn't
+// call the declareMurderer function, and should be dealing with the first
+// 'let murderer' instead.
+
+
+// EPISODE FOUR
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// I would expect Miss Scarlet, Professor Plum and Colonel Mustard to be
+// returned, as the declareAllSuspects function has been called, which changes
+// the third suspect to Colonel Mustard. I would expect him to be named in the
+// second log also. I WAS WRONG (because I didn't read it properly and the
+// second log never deals with the suspects variable, which is the only one
+// that calls the declareAllSuspects function as mentioned before.)
+
+
+// EPISODE FIVE
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// I would expect "The weapon is the Revolver" to be returned.
+
+
+// EPISODE SIX
+
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I would expect 'The murderer is Mrs. White' to be returned. What a plot
+// twist!! I'm assuming the only reason this is the case is because the
+// plotTwist function is actually called within the changeMurderer function.
+// Otherwise it wouldn't do anything, and just return Mr. Green?
+// Edit: Yeah, I checked. :D
+
+
+// EPISODE SEVEN
+
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I would expect 'The murderer is Miss Scarlet' to be returned. I WAS WRONG.
+// I wasn't very confident about this one. Hmm. Even knowing the right answer
+// isn't helping!
+
+
+// EPISODE EIGHT
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+//
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+//
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+//
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+//
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+//
+//   plotTwist('Dining Room');
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+//
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// I would expect 'The weapon is Candle Stick' to be returned, as the conditions
+// for each if statement are being met as you pass through the functions.
+
+
+// EPISODE NINE
+
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I would expect 'The murderer is Professor Plum' to be returned, because I
+// don't get 'let' very well and don't trust it in that 'if' statement.
+// Cool, I was right but I'm not sure how. :D
